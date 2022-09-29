22c6f91fc75a4fcc6d4bcb3fbb19887bd036e7d0
Sboti03 <somogyi.botond.munka@gmail.com>
2022-09-29 14:04:07 +0200

    Main activity layout kész + init kód

--- app/src/main/java/com/example/menu_14t/MainActivity.java
+++ app/src/main/java/com/example/menu_14t/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.menu_14t;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnSubmit;
+    private EditText editTextInput;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
     }
+
+
+    private void init() {
+        btnSubmit = findViewById(R.id.btnSubmit);
+        editTextInput = findViewById(R.id.editTextInput);
+    }
+
 }
---
+++ app/src/main/java/com/example/menu_14t/masodikActivity.java
@@ -0,0 +1,14 @@
+package com.example.menu_14t;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class masodikActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_masodik);
+    }
+}
